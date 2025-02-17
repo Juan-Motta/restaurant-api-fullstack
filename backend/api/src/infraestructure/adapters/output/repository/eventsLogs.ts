6f1fa5591410665ebe3ea75ab049f66fbb79523d
Juan <j_andreslm@hotmail.com>
2025-02-17 16:43:20 -0500

    refactor: Improve formatting and structure of event logging repository methods

--- backend/api/src/infraestructure/adapters/output/repository/eventsLogs.ts
+++ backend/api/src/infraestructure/adapters/output/repository/eventsLogs.ts
@@ -8,9 +8,17 @@ export class EventsLogsRepository implements IEventsLogsRepository {
         this.client = client
     }
 
-    async create(event: string, message: Record<string, any>, state: string, error: string | null | undefined): Promise<void> {
-        await this.client.query(`
+    async create(
+        event: string,
+        message: Record<string, any>,
+        state: string,
+        error: string | null | undefined
+    ): Promise<void> {
+        await this.client.query(
+            `
             INSERT INTO event_logs (event, message, state, error) VALUES ($1, $2, $3, $4);
-        `, [event, message, state, error || null])
+        `,
+            [event, message, state, error || null]
+        )
     }
 }
